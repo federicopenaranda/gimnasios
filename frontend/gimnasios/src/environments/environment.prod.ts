47688b7b415a643d7d750b3003701e54da98b303
Federico Peñaranda <federicopenaranda@gmail.com>
2024-12-11 22:05:28 -0300

    Railway deploy

--- frontend/gimnasios/src/environments/environment.prod.ts
+++ frontend/gimnasios/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'endearing-clarity-production.up.railway.app',
+  API_URL: 'https://endearing-clarity-production.up.railway.app',
   WEB_URL: 'https://www.kataguy.dev/gimnasios'
 }
